07ae475a6e260c55eb70a3e5ece9893f05479b0a
Rob Lazzurs <rob@lazzurs.org>
2019-12-09 00:47:09 +0000

    Migrating the provider to the plugin SDK.
    Migrated the provider to the plugin SDK using the automated migration
    tool. Build runs, no issues.
    
    Adding vendor additions

--- netlify/resource_deploy_key.go
+++ netlify/resource_deploy_key.go
@@ -1,7 +1,7 @@
 package netlify
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/netlify/open-api/go/plumbing/operations"
 )
 
